654d4c9ea4b3773101bf6a3c2ee6786ee2f9c3bb
Rene Richter <Richterrettich@gmail.com>
2015-12-20 23:57:50 +0100

    Progress

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./gradlew clean build
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_REPO_SLUG}" = "InteractiveLecture/string-compiler" ] ; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_REPO_SLUG}" = "InteractiveLecture/java-string-compiler" ] ; then
   ./gradlew bintrayUpload
 fi
